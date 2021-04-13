bfaf59f55cf2ac80a483e62d7ff44e59f8d1c066
Pawarit Thareechan <root@lckyn.com>
2021-04-13 23:01:12 +0700

    add title

--- Estimating Pi using Monte Carlo/video.py
+++ Estimating Pi using Monte Carlo/video.py
@@ -20,20 +20,71 @@ class Grid(Scene):
 
         self.add(circle)
         self.add(square)
-
         self.wait(1)
 
-        point = []
-        for i in range(2000):
+        point_text, point_number = point_label = VGroup(
+            Text("point : "),
+            DecimalNumber(
+                0,
+                show_ellipsis=True,
+                num_decimal_places=4
+            )
+        )
+
+        in_text, in_number = in_label = VGroup(
+            Text("In a circle : "),
+            DecimalNumber(0)
+        )
+
+        pi_text, pi_number = pi_label = VGroup(
+            Text("PI : "),
+            DecimalNumber(0)
+        )
+        point_label.arrange(RIGHT)
+        in_label.arrange(RIGHT)
+        pi_label.arrange(RIGHT)
+
+        point_label.move_to((2, -1, 0))
+        in_label.move_to((2, 0, 0))
+        pi_label.move_to((2, 1, 0))
+
+        self.add(point_label, in_label, pi_label)
+
+        # always(count_number.set_value, self.time())
+        # count_number.add_updater(lambda m: m.set_value(ran.random()))
+        count_all = 0
+        c = 0
+        apx_pi = 0
+        point_number.add_updater(lambda m: m.set_value(count_all))
+        in_number.add_updater(lambda m: m.set_value(c))
+        pi_number.add_updater(lambda m: m.set_value(apx_pi))
+
+        ran.seed(1)
+
+        for i in range(200):
             pos = (-6 + ran.random() * 4, -2 + ran.random() * 4, 0)
             if((pos[0] + 4) ** 2 + pos[1] ** 2 < 4):
-                d = Dot(color=RED)
+                d = Dot(color=RED, radius = 0.25)
+                c += 1
             else:
-                d = Dot(color=GREEN)
+                d = Dot(color=GREEN, radius = 0.25)
             d.move_to(pos)
-            point.append(d)
-            # self.play(ShowCreation(d, run_time=0.1))
+            self.play(Create(d, run_time=0.05))
+            count_all = i
+            apx_pi = c/(i+1) * 4
 
-        group = VGroup(*point)
-        self.play(ShowIncreasingSubsets(group, run_time=3.0))
+        # group = VGroup(*point)
+        # group2 = VGroup(*count)
+        # group2.next_to(count_text, RIGHT)
+
+        # i = 5
+        # # def temp(n):
+        # #     global i
+        # #     i += 1
+        # #     n.set_value(i)
+
+        # self.play(
+        #     ShowIncreasingSubsets(group, run_time=20.0),
+            # ApplyFunction(temp, count_number)
+            #   )
         self.wait(2)
