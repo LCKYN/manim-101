b908eaa19e137484e857e89064643d13f16491f0
Pawarit Thareechan <root@lckyn.com>
2021-04-17 09:48:42 +0700

    update add new project

---
+++ Chaos Game Sierpinski Triangle/temp.py
@@ -0,0 +1,10 @@
+from manim import *
+import numpy as np
+import random as ran
+
+
+class MonteCarlo(Scene):
+    def construct(self):
+        dot = Dot(radius = 0.02)
+        self.add(line)
+        self.wait()
