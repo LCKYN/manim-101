b1f93b65da76b5a965ad9e2ebc63929249f693dd
Pawarit Thareechan <root@lckyn.com>
2021-04-18 17:10:50 +0700

    update

--- Chaos Game Sierpinski Sierpinski carpet/video.py
+++ Chaos Game Sierpinski Sierpinski carpet/video.py
@@ -2,51 +2,51 @@ from manim import *
 import numpy as np
 import random as ran
 
-
+def lerp(v, d):
+    return v[0] * (1 - d) + v[1] * d
 class SierpinskiTriangle(Scene):
+
+
     def construct(self):
         self.wait()
 
-        dot = Dot(color=RED, radius=0.2).move_to((-3, 3, 0))
-        self.add(dot)
-        dot = Dot(color=GREEN, radius=0.2).move_to((-3, -3, 0))
-        self.add(dot)
-        dot = Dot(color=BLUE, radius=0.2).move_to((3, -3, 0))
-        self.add(dot)
-        # dot = Dot(color=BLUE, radius=0.2).move_to((-3, 3, 0))
-        # self.add(dot)
-
-        self.wait()
+        initpoint = [(-3, -3, 0),
+                     (-3,  0, 0),
+                     (-3,  3, 0),
+                     (0, -3, 0),
+                     (0,  3, 0),
+                     (3, -3, 0),
+                     (3,  0, 0),
+                     (3,  3, 0)]
 
         allDot = set()
-        allDot.add((-3, 3, 0))
-        allDot.add((-3, -3, 0))
-        allDot.add((3, -3, 0))
-        # allDot.add((-3, 3, 0))
 
-        r = 0.0125
+        c = [BLUE, TEAL, GREEN, YELLOW, GOLD, RED, MAROON, PURPLE]
+        for i in range(len(initpoint)):
+            allDot.add(initpoint[i])
+
+            dot = Dot(color=c[i], radius=0.2).move_to(initpoint[i])
+            self.add(dot)
+        self.wait()
+
+        r = 0.02
+
+        ratio = 2/3
 
         for i in allDot:
             dot = Dot(radius=r).move_to((i))
             self.add(dot)
         self.wait()
 
-        for i in range(7):
+        for i in range(5):
             tempset = set()
 
             print(i, len(allDot))
 
             for d in allDot:
-
-                meanpoint1 = ((d[0] - 3)/2, (d[1] + 3)/2, 0)
-                meanpoint2 = ((d[0] - 3)/2, (d[1] - 3)/2, 0)
-                meanpoint3 = ((d[0] + 3)/2, (d[1] - 3)/2, 0)
-                # meanpoint4 = ((d[0] - 3)*2/3, (d[1] + 3)*2/3, 0)
-
-                tempset.add(meanpoint1)
-                tempset.add(meanpoint2)
-                tempset.add(meanpoint3)
-                # tempset.add(meanpoint4)
+                for j in range(len(initpoint)):
+                    meanpoint = (lerp([d[0], initpoint[j][0]], ratio), lerp([d[1], initpoint[j][1]], ratio), 0)
+                    tempset.add(meanpoint)
 
             for count, m in enumerate(tempset):
                 print(count, " : ", len(tempset), end="\r")
