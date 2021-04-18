2fb784d2e5231c59e0173bf2810ce11de419f53f
Pawarit Thareechan <root@lckyn.com>
2021-04-18 09:51:31 +0700

    add step by step

---
+++ Chaos Game Sierpinski Triangle 2/video.py
@@ -0,0 +1,55 @@
+from manim import *
+import numpy as np
+import random as ran
+
+
+class SierpinskiTriangle(Scene):
+    def construct(self):
+        self.wait()
+
+        dot = Dot(color=RED, radius=0.2).move_to((0, 3, 0))
+        self.add(dot)
+        dot = Dot(color=GREEN, radius=0.2).move_to((-3, -3, 0))
+        self.add(dot)
+        dot = Dot(color=BLUE, radius=0.2).move_to((3, -3, 0))
+        self.add(dot)
+
+        self.wait()
+
+        allDot = set()
+        allDot.add((0, 3, 0))
+        allDot.add((-3, -3, 0))
+        allDot.add((3, -3, 0))
+
+        r = 0.01
+
+        for i in allDot :
+            dot = Dot(radius=r).move_to((i))
+            self.add(dot)
+        self.wait()
+
+
+        for i in range(10):
+            tempset = set()
+
+            print(len(allDot))
+
+            for d in allDot:
+                
+                meanpoint1 = ((d[0] + 0)/2, (d[1] + 3)/2, 0)
+                meanpoint2 = ((d[0] - 3)/2, (d[1] - 3)/2, 0)
+                meanpoint3 = ((d[0] + 3)/2, (d[1] - 3)/2, 0)
+
+                tempset.add(meanpoint1)
+                tempset.add(meanpoint2)
+                tempset.add(meanpoint3)
+
+            for m in tempset:
+                allDot.add(m)
+                dot = Dot(radius=r).move_to((m))
+                self.add(dot)
+
+
+            self.wait()
+
+        self.wait()
